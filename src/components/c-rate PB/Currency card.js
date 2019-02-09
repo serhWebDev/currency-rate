88347b51d9aacfd230da784517bf44102a3455d7
Serhii <serhiivekshyn@gmail.com>
2019-02-09 20:50:29 +0200

    3 start, menu (minor changes)

--- src/components/c-rate PB/Currency card.js
+++ src/components/c-rate PB/Currency card.js
@@ -1,29 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class CurrencyCard extends Component {
-    render() {
-        return (
-            <div className={'card container'} style={CurrencyCardStyle}>
-                <h5>
-                    <span>Валюта: </span>
-                    <span style={dataStyle}>{this.props.currency.ccy}</span>
-                </h5>
-                <p>
-                    <span>Национальная валюта: </span>
-                    <span style={dataStyle}>{this.props.currency.base_ccy}</span>
-                </p>
-                <p>
-                    <span>Курс покупки: </span>
-                    <span style={dataStyle}>{this.props.currency.buy}</span>
-                </p>
-                <p>
-                    <span>Курс продажи: </span>
-                    <span style={dataStyle}>{this.props.currency.sale}</span>
-                </p>
-            </div>
-        );
-    }
-}
+const CurrencyCard = props => (
+    <div className={'card container'} style={CurrencyCardStyle}>
+        <h5>
+            <span>Валюта: </span>
+            <span style={dataStyle}>{props.currency.ccy}</span>
+        </h5>
+        <p>
+            <span>Национальная валюта: </span>
+            <span style={dataStyle}>{props.currency.base_ccy}</span>
+        </p>
+        <p>
+            <span>Курс покупки: </span>
+            <span style={dataStyle}>{props.currency.buy}</span>
+        </p>
+        <p>
+            <span>Курс продажи: </span>
+            <span style={dataStyle}>{props.currency.sale}</span>
+        </p>
+    </div>
+);
 
 const dataStyle = {
     float: 'right'
